b02669d1a39e92705443acd9c89517dde0956641
Gerson Umanzor <gbumanzor@outlook.com>
2021-05-19 08:24:36 -0600

    Feat: Add views.

---
+++ src/views/Characters.vue
@@ -0,0 +1,16 @@
+<template>
+  <CharacterList />
+</template>
+
+<script>
+import CharacterList from '@/components/characters/CharacterList';
+
+export default {
+  name: 'Characters',
+  components: { CharacterList },
+};
+</script>
+
+<style scoped>
+
+</style>
